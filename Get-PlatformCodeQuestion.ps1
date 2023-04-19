5cfd1d72215c4ef5c4f88351f9d4ef2bef54e82d
Andrew Alkema <andrew.alkema@d2l.com>
2023-04-19 09:31:44 -0400

    add an if

--- Get-PlatformCodeQuestion.ps1
+++ Get-PlatformCodeQuestion.ps1
@@ -3,11 +3,13 @@ We have a workflow we run every morning we'd like to automate.
 
 We have a CSV available here we need to download: https://freetestdata.com/wp-content/uploads/2021/09/Free_Test_Data_200KB_CSV-1.csv
 
-Once we have the CSV, we need to extract all males from France - there was a data entry error with them we need to fix each morning.
+There was a strange data entry issue we need to correct every day.  On days where the date is even, the system messes up French males names.
+On days where the date is odd, it messes up French female names.
 
-We need to save a csv file with just the male, French names with an "s" added to the end of each name.
+We need to extract the male French names on even dates, and French female names on odd dates.  On those even dates, we extract the French male
+names only, and add an "s" on the end.  On the odd dates, we extract the French female names only, and add a "y".  
 
-For example, if this was our CSV:
+For example, if this was our CSV on the 4th of the month:
 1249	Felisaas	Male	30	21/05/2018	France
 1250	Demetas	Female	31	15/10/2020	Great Britain
 1251	Jeromyw	Female	32	16/08/2019	France
