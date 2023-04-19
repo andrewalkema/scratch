1a057c6b24ead3a70ef5c3f7475f150b0e4495e9
Andrew Alkema <andrew.alkema@d2l.com>
2023-04-19 11:03:07 -0400

    suggested solution

--- Get-PlatformCodeQuestion.ps1
+++ Get-PlatformCodeQuestion.ps1
@@ -7,7 +7,8 @@ There was a strange data entry issue we need to correct every day.  On days wher
 On days where the date is odd, it messes up French female names.
 
 We need to extract the male French names on even dates, and French female names on odd dates.  On those even dates, we extract the French male
-names only, and add an "s" on the end.  On the odd dates, we extract the French female names only, and add a "y".  
+names only, and add an "s" on the end.  On the odd dates, we extract the French female names only, and add a "y".  We'll save the file
+in the local directory called "employees.csv"
 
 For example, if this was our CSV on the 4th of the month:
 1249	Felisaas	Male	30	21/05/2018	France
@@ -21,3 +22,29 @@ We would need to save a csv file like this:
 
 #>
 
+# Sample solution
+
+$employees = Invoke-WebRequest -Uri "https://freetestdata.com/wp-content/uploads/2021/09/Free_Test_Data_200KB_CSV-1.csv" | ConvertFrom-Csv
+
+$frenchEmployees = $employees | Where-Object { $_.Country -eq "France" }
+
+$date = (Get-Date).Day
+
+if ( $date % 2 -eq 0 ) {
+    # Date is even
+    $frenchMales = $frenchEmployees | Where-Object { $_.Gender -eq "Male" }
+    foreach ( $employee in $frenchMales ) {
+        $employee.Name = $employee.Name + "s"
+    }
+    Export-Csv -InputObject $frenchMales -Path "./employees.csv"
+} else {
+    # Date is odd
+    $frenchFemales = $frenchEmployees | Where-Object { $_.Gender -eq "Female" }
+    foreach ( $employee in $frenchFemales ) {
+        $employee.Name = $employee.Name + "y"
+    }
+
+    $frenchFemales | Export-Csv -Path "./employees.csv"
+}
+
+# Could make a function out of 33->45?  
